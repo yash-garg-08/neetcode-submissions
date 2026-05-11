17c738aa2c60484bf9af1aa304ddf51e42d09063
Yash Garg <60481835+yash-garg-08@users.noreply.github.com>
2026-05-11 18:56:30 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.cpp
@@ -0,0 +1,41 @@
+class Solution {
+   public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        // using stack
+        int n = temperatures.size();
+        stack<pair<int, int>> s;
+        vector<int> res(n);
+
+        for (int i = 0; i < n; i++) {
+            if(!s.empty()){
+                while(!s.empty() && temperatures[i] > s.top().first){
+                    res[s.top().second] = i - s.top().second;
+                    s.pop();
+                }
+                s.push({temperatures[i], i});
+            }else{
+                s.push({temperatures[i], i});
+            }
+        }
+        return res;
+
+        // Using brute force
+        // int n = temperatures.size();
+        // vector<int> res;
+        // for (int i = 0; i < n; i++) {
+        //     int current = temperatures[i];
+        //     int flag = 0;
+        //     for (int j = i + 1; j < n; j++) {
+        //         if (temperatures[j] > temperatures[i]) {
+        //             res.push_back(j - i);
+        //             flag = 1;
+        //             break;
+        //         }
+        //     }
+        //     if (flag == 0) {
+        //         res.push_back(0);
+        //     }
+        // }
+        // return res;
+    }
+};
